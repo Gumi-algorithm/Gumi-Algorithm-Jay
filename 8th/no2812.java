32f43ffdae3bb834bbd1aa78fef6d50c1c4b0715
Jay_Lee <57380565+edenist-x-x@users.noreply.github.com>
2021-02-19 20:46:51 +0900

    8th BOJ_2812 크게 만들기 ( 반례를 못찾겠.. )

---
+++ 8th/no2812.java
@@ -0,0 +1,40 @@
+package BojAlgorithms;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class no2812 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int delete = Integer.parseInt(st.nextToken());
+
+        //삭제할 원소 카운팅
+        int count = 0;
+        String[] num = br.readLine().split("");
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            // 현재의 값이 스택 안의 값보다 크면 꺼내고 삭제
+            while (!stack.isEmpty() && count < delete && Integer.parseInt(stack.peek()) < Integer.parseInt(num[i])) {
+                stack.pop();
+                count++; // 카운트 증가
+            }
+            // 현재 위치의 값 스택에 넣기
+            stack.push(num[i]);
+        }
+
+        for(int i=0; i<stack.size(); i++){
+            sb.append(stack.get(i));
+        }
+
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
+    }
+}
