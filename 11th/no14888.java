1dddc4bb4b26e7216cfb22b6d4344d56c605f626
Jay_Lee <57380565+edenist-x-x@users.noreply.github.com>
2021-03-07 20:38:09 +0900

    Update no14888.java

--- 11th/no14888.java
+++ 11th/no14888.java
@@ -7,15 +7,16 @@ import java.util.StringTokenizer;
 
 public class no14888 {
     static int N;
-    static int[] nums;
-    static int[] o = new int[4];
-    static int max = Integer.MIN_VALUE;
-    static int min = Integer.MAX_VALUE;
+    static int[] nums; // 숫자들 
+    static int[] o = new int[4]; // 연산자 배열 
+    static int max = Integer.MIN_VALUE; // 최대값 
+    static int min = Integer.MAX_VALUE; // 최소값 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        // 입려 
         N = Integer.parseInt(br.readLine());
         nums = new int[N];
 
@@ -29,21 +30,22 @@ public class no14888 {
             o[i] = Integer.parseInt(st.nextToken());
         }
 
+        // dfs 실행 
         dfs(nums[0], 1);
         System.out.println(max);
         System.out.println(min);
 
     }
     static void dfs(int n, int idx){
-        if(idx == N){
-            max = Math.max(max, n);
-            min = Math.min(min, n);
+        if(idx == N){ // 종료조건 : 매개변수이 인덱스가 N까지 갔다면 재귀 종료 
+            max = Math.max(max, n); // 연산 최대값 
+            min = Math.min(min, n); // 연산 최소값 
         }
-        for(int i = 0; i < 4; i++){
-            if(o[i] > 0){
-                o[i]--;
+        for(int i = 0; i < 4; i++){ 
+            if(o[i] > 0){ // 연산자의 개수가 1보다 크다면 
+                o[i]--; // 하나를 빼줌 
 
-                switch(i){
+                switch(i){ // switch 문을 통해서 0 : 덧셈, 1 : 뺄셈, 2 : 곱셈, 3 : 나누기 수행하면서 재귀 반복 
                     case 0:
                         dfs(n + nums[idx], idx + 1);
                         break;
@@ -57,7 +59,7 @@ public class no14888 {
                         dfs(n / nums[idx], idx + 1);
                         break;
                 }
-                o[i]++;
+                o[i]++; // 빼줬더 연산자, 재귀 종료 시 초기화해주기. 여기서 시간많이잡음.. 
             }
         }
     }
